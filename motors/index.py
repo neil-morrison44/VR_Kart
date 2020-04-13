81435efbf25b54907186b96858dae205b4da91a5
Neil Morrison <neil.morrison44@googlemail.com>
2020-04-13 16:57:42 +0100

    add motor control

--- motors/index.py
+++ motors/index.py
@@ -1,7 +1,8 @@
 import socket
 import time
 import os
-# import pigpio
+import json
+from gpiozero import Motor, OutputDevice
 
 socket_path = '/tmp/uv4l.socket'
 
@@ -18,12 +19,25 @@ s.bind(socket_path)
 s.listen(1)
 
 
+def setupMotors():
+    motor1 = Motor(24, 27)
+    motor1_enable = OutputDevice(5, initial_value=1)
+    motor2 = Motor(6, 22)
+    motor2_enable = OutputDevice(17, initial_value=1)
+    motor3 = Motor(23, 16)
+    motor3_enable = OutputDevice(12, initial_value=1)
+    motor4 = Motor(13, 18)
+    motor4_enable = OutputDevice(25, initial_value=1)
+    return [motor1, motor2, motor3, motor4]
+
+
+motors = setupMotors()
+
+
 def cleanup():
     print("cleanup")
-    # pi.set_servo_pulsewidth(ROLL_PIN, 0)
-    # pi.set_servo_pulsewidth(PITCH_PIN, 0)
-    # pi.set_servo_pulsewidth(YAW_PIN, 0)
-    # pi.stop()
+    for motor in motors:
+        motor.stop()
 
 
 while True:
@@ -32,22 +46,16 @@ while True:
     print('client_address %s', client_address)
     try:
         print('established connection with', client_address)
-
-        # pi = pigpio.pi()
-
-        # rollPulsewidth = MID_PW
-        # pitchPulsewidth = MID_PW
-        # yawPulsewidth = MID_PW
-
-        # pi.set_servo_pulsewidth(ROLL_PIN, rollPulsewidth)
-        # pi.set_servo_pulsewidth(PITCH_PIN, pitchPulsewidth)
-        # pi.set_servo_pulsewidth(YAW_PIN, yawPulsewidth)
-
         while True:
             data = connection.recv(16)
             print('received message"%s"', data)
 
             time.sleep(0.01)
+
+            values = json.loads(data)
+
+            for i in range(len(values)):
+                motors[i].value = values[i]
 
             if data:
                 print('echo data to client')
