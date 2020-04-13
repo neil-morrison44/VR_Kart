2fb88f559a4811ab2bb85aa1dbcb05661cb47792
Neil Morrison <neil.morrison44@googlemail.com>
2020-04-13 18:42:07 +0100

    delete js, start both processes

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,7 +1,14 @@
+python3 ./motors/index.py & PIDPY=$!
+
+sleep 5
+
 uv4l --enable-server --driver raspicam \
 --server-option '--use-ssl=yes' --server-option '--ssl-private-key-file=/media/certs/server.key' \
 --server-option '--ssl-certificate-file=/media/certs/server.crt' --verbosity=8 -f \
 --server-option '--enable-webrtc-video=yes' --server-option '–-enable-webrtc=yes' --auto-video_nr on \
 --server-option '--webrtc-hw-vcodec-maxbitrate=1600' --server-option '--webrtc-hw-vcodec-minbitrate=200' \
 --server-option '--webrtc-hw-vcodec-startbitrate=200' --server-option '--webrtc-receive-video=no' \
---server-option '--max-streams=1' --server-option '--max-threads=2' --server-option '--webrtc-datachannel-socket=/tmp/uv4l.socket'
+--server-option '--max-streams=1' --server-option '--max-threads=2' --server-option '--webrtc-datachannel-socket=/tmp/uv4l.socket' & PIDUV=$!
+
+wait $PIDUV
+wait $PIDPY
